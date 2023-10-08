ae49be2d680828fcabdfb2f841f8afdc8d3c530c
BrianMutheeGatitu <brian.gatitu@student.moringaschool.com>
2023-10-09 01:53:33 +0300

    completed assingmnet

--- main.js
+++ main.js
@@ -3,14 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let message = document.getElementById("modal")
+message.setAttribute('class', 'hidden')
 
+like = document.getElementsByClassName("like")
 
+for(let element of like){
+  element.addEventListener("click", ()=>{
+    mimicServerCall()
+    .then(()=>{
+      if (element.lastChild.innerHTML == EMPTY_HEART) {
+        element.lastChild.setAttribute('class', 'activated-heart')
+        element.lastChild.innerHTML = FULL_HEART;
+      }
+      else {
+        element.lastChild.removeAttribute('class', 'activated-heart');
+        element.lastChild.innerHTML = EMPTY_HEART;
+      }
+
+    })
+    .catch((error)=>{
+      message.removeAttribute('class','hidden')
+      message.lastChild.textContent = error.message
+      setTimeout(()=>{message.setAttribute('class', 'hidden')}, 3000)
+    })
+
+  })
+}
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
-
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
